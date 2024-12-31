333cb33ceede8d0adea696dc9d8e9508a0c1f94d
mshaner97 <mshaner97@icloud.com>
2024-12-30 19:30:36 -0600

    solitare card work

--- src/Classwork/PartnerProject/solitaire.js
+++ src/Classwork/PartnerProject/solitaire.js
@@ -1,14 +1,59 @@
+class Card {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+        this.faceUp = false;
+    }
+    flip() {
+        this.faceUp = !this.faceUp;
+    }
+    getSuitSymbol() {
+        // Convert suit name to symbol
+        const symbols = {
+            'hearts': '♥',
+            'diamonds': '♦',
+            'clubs': '♣',
+            'spades': '♠'
+        };
+        return symbols[this.suit];
+    }
+
+    createCardElement() {
+        const card = document.createElement('div');
+        card.className = `card ${this.suit}`;
+        
+        if (this.faceUp) {
+            card.innerHTML = `
+                <div class="cardContent">
+                    <div class="card-value top-value">${this.value}</div>
+                    <div class="center-suit">${this.getSuitSymbol()}</div>
+                    <div class="card-value bottom-value">${this.value}</div>
+                </div>
+            `;
+        } else {
+            card.classList.add('facedown');
+        }
+        
+        return card;
+    }
+}
 // Creating the deck of cards for array sorting
-function createDeck() {
-    const suits = ['♠', '♥', '♦', '♣'];
+class Deck {
+    constructor() {
+        this.cards = this.createDeck();
+    }
+    createDeck() {
+    const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
     const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     const deck = [];
+    
     for (let suit of suits) {
         for (let value of values) {
-            deck.push({suit, value});
+            deck.push(new Card(suit, value));
         }
     }
     return deck;
+}
 }
 // deck shuffling
 function shuffleDeck(deck) {
@@ -27,11 +72,58 @@ function dealCards(deck) {
     }
     return tableau;
 }
+function flipTopCards(tableau) {
+    tableau.forEach(pile => {
+        if (pile.length > 0) {
+            pile[pile.length - 1].flip();
+        }
+    });
+}
+function updateTableau(tableau) {
+    tableau.forEach((pile, i) => {
+        const pileElement = document.getElementById(`tableau-${i}`);
+        pileElement.innerHTML = '';
+        pile.forEach(card => {
+            pileElement.appendChild(card.createCardElement());
+        });
+    });
+}
+
+function updateFoundations(foundations) {
+    foundations.forEach((pile, i) => {
+        const pileElement = document.getElementById(`foundation-${i}`);
+        pileElement.innerHTML = '';
+        if (pile.length > 0) {
+            pileElement.appendChild(pile[pile.length - 1].createCardElement());
+        }
+    });
+}
+
+function updateStock(stock) {
+    const stockElement = document.getElementById('stock');
+    stockElement.innerHTML = stock.length > 0 ? '<div class="card facedown"></div>' : '';
+}
+
+function updateWaste(waste) {
+    const wasteElement = document.getElementById('waste');
+    wasteElement.innerHTML = '';
+    if (waste.length > 0) {
+        wasteElement.appendChild(waste[waste.length - 1].createCardElement());
+    }
+}
+
+function updateGameBoard(game) {
+    updateTableau(game.tableau);
+    updateFoundations(game.foundations);
+    updateStock(game.stock);
+    updateWaste(game.waste);
+}
 // Beginning the game
 function startGame() {
-    const deck = createDeck();
+    const deck = new Deck().cards;
     shuffleDeck(deck);
     const tableau = dealCards(deck);
+    flipTopCards(tableau);
     const stock = deck;
     const foundations = Array(4).fill().map(() => []);
     const waste = [];
@@ -39,6 +131,7 @@ function startGame() {
     return { tableau, stock, foundations, waste };
 }
 const game = startGame();
+updateGameBoard(game);
 console.log(game);
 // Check if card can be moved to the foundation
 function canMoveToFoundation(card, foundation) {
@@ -52,13 +145,13 @@ function canMoveToFoundation(card, foundation) {
 }
 // Check if card can be moved to the tableau
 function canMoveToTableau(card, tableauPile) {
-    if (tablueaPile.length === 0) {
+    if (tableauPile.length === 0) {
         return card.value === 'K';
     }
-    const topCard = tablueaPile[tableauPile.length - 1];
+    const topCard = tableauPile[tableauPile.length - 1];
     const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  const redSuits = ['♥', '♦'];
-  const blackSuits = ['♠', '♣'];
+  const redSuits = ['hearts', 'diamonds'];
+  const blackSuits = ['spades', 'clubs'];
   return (
     (redSuits.includes(card.suit) && blackSuits.includes(topCard.suit)
 ||
@@ -66,4 +159,27 @@ function canMoveToTableau(card, tableauPile) {
 &&
     values.indexOf(card.value) === values.indexOf(topCard.value) - 1
     );
+}
+// Ability to move cards between piles
+function moveCard(fromPile, toPile, cardIndex) {
+    const movedCards = fromPile.splice(cardIndex);
+    toPile.push(...movedCards);
+    
+    if (fromPile.length > 0 && !fromPile[fromPile.length - 1].faceUp) {
+        fromPile[fromPile.length - 1].flip();
+    }
+
+    updateGameBoard(game);
+}
+function drawCard(game) {
+    if (game.stock.length > 0) {
+        const card = game.stock.pop();
+        card.flip();
+        game.waste.push(card);
+    } else if (game.waste.length > 0) {
+        game.stock = game.waste.reverse();
+        game.waste = [];
+        game.stock.forEach(card => card.flip());
+    }
+    updateGameBoard(game);
 }
